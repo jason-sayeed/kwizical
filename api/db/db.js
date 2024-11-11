476339b57a031f8d9aa8d71c1411a1c576bda2fe
Jason <jason.sayeed@gmail.com>
2024-11-11 15:01:31 +0000

    updated uri in db file

--- api/db/db.js
+++ api/db/db.js
@@ -4,7 +4,8 @@ const connectToDatabase = async () => {
   // Line 6 (below) defines the connection URI for connecting to the MongoDB Atlas cluster (cloud-hosted MongoDB service) using Mongoose and replaces line 8 when needed.
   // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@kwizical.xpmi3dw.mongodb.net/?retryWrites=true&w=majority&appName=kwizical`;
 
-  const uri = "mongodb://localhost:27017/kwizical";
+  // const uri = "mongodb://localhost:27017/kwizical";
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@server-testing.sqpii.mongodb.net/kwizical-game?retryWrites=true&w=majority&appName=server-testing`;
 
   const clientOptions = {
     serverApi: { version: "1", strict: true, deprecationErrors: true },
